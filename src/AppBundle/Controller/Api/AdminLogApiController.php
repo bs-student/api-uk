78d95efd7c5d27fd09a1b7458429e5b3efef4db0
sujit13666 <sujit13666@gmail.com>
2016-11-10 19:25:46 +0600

    Log Module Added

---
+++ src/AppBundle/Controller/Api/AdminLogApiController.php
@@ -0,0 +1,365 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use AppBundle\Entity\Log;
+use AppBundle\Form\Type\LogType;
+use AppBundle\Validator\Constraints\UsernameConstraints;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\Type\UserType;
+use AppBundle\Entity\User;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\Extension\Validator\ViolationMapper\ViolationMapper;
+use Symfony\Component\Validator\ConstraintViolation;
+
+
+class AdminUserApiController extends Controller
+{
+
+    /**
+     * Get all Non Approved users
+     *
+     */
+    public function getAllNonApprovedUserAction(Request $request){
+
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+
+        if(in_array('ROLE_ADMIN_USER',$user->getRoles(),true)){
+
+            $content = $request->getContent();
+            $data = json_decode($content, true);
+            $em = $this->getDoctrine()->getManager();
+            $userRepo=$em->getRepository('AppBundle:User');
+
+
+            $pageSize = $data["pageSize"];
+            $searchQuery = filter_var($data["searchQuery"], FILTER_SANITIZE_STRING);
+            $emailQuery = filter_var($data["emailQuery"], FILTER_SANITIZE_STRING);
+            $fullNameQuery = filter_var($data["fullNameQuery"], FILTER_SANITIZE_STRING);
+            $enabledQuery = $data["enabledQuery"];
+            $pageNumber = $data["pageNumber"];
+            $sort = $data["sort"];
+
+
+
+            $totalNumber = $userRepo->getNonApprovedUserSearchNumber($searchQuery,$emailQuery,$fullNameQuery,$enabledQuery);
+            $users = $userRepo->getNonApprovedUserSearchResult($searchQuery,$emailQuery,$fullNameQuery,$enabledQuery, $pageNumber, $pageSize,$sort);
+
+            for($i=0;$i<count($users);$i++){
+                $users[$i]['registrationDateTime'] =$users[$i]['registrationDateTime']->format('g:i A, d M Y');
+            }
+
+
+            $data = array(
+                'totalUsers' => $users ,
+                'totalNumber' => $totalNumber
+            );
+
+            return $this->_createJsonResponse('success', array('successData'=>array('users'=>$data)), 200);
+        }else{
+            return $this->_createJsonResponse('error', array('errorTitle'=>"You are not authorized to see this page."), 400);
+        }
+
+
+    }
+
+    /**
+     * Get all Approved users
+     *
+     */
+    public function getAllApprovedUserAction(Request $request){
+
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+
+        if(in_array('ROLE_ADMIN_USER',$user->getRoles(),true)){
+
+            $content = $request->getContent();
+            $data = json_decode($content, true);
+            $em = $this->getDoctrine()->getManager();
+            $userRepo=$em->getRepository('AppBundle:User');
+
+
+            $pageSize = $data["pageSize"];
+            $searchQuery = filter_var($data["searchQuery"], FILTER_SANITIZE_STRING);
+            $emailQuery = filter_var($data["emailQuery"], FILTER_SANITIZE_STRING);
+            $fullNameQuery = filter_var($data["fullNameQuery"], FILTER_SANITIZE_STRING);
+            $universityNameQuery = filter_var($data["universityNameQuery"], FILTER_SANITIZE_STRING);
+            $campusNameQuery = filter_var($data["campusNameQuery"], FILTER_SANITIZE_STRING);
+            $enabledQuery = $data["enabledQuery"];
+            $pageNumber = $data["pageNumber"];
+            $sort = $data["sort"];
+
+
+
+            $totalNumber = $userRepo->getApprovedUserSearchNumber($searchQuery,$emailQuery,$fullNameQuery,$universityNameQuery,$campusNameQuery,$enabledQuery);
+            $users = $userRepo->getApprovedUserSearchResult($searchQuery,$emailQuery,$fullNameQuery,$universityNameQuery,$campusNameQuery,$enabledQuery, $pageNumber, $pageSize,$sort);
+
+            for($i=0;$i<count($users);$i++){
+                $users[$i]['registrationDateTime'] =$users[$i]['registrationDateTime']->format('g:i A, d M Y');
+            }
+
+            $data = array(
+                'totalUsers' => $users ,
+                'totalNumber' => $totalNumber
+            );
+
+            return $this->_createJsonResponse('success', array('successData'=>array('users'=>$data)), 200);
+        }else{
+            return $this->_createJsonResponse('error', array('errorTitle'=>"You are not authorized to see this page."), 400);
+        }
+
+
+    }
+
+    /**
+     * Get all Admin Users
+     *
+     */
+    public function getAllAdminUserAction(Request $request){
+
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+
+        if(in_array('ROLE_ADMIN_USER',$user->getRoles(),true)){
+
+            $content = $request->getContent();
+            $data = json_decode($content, true);
+            $em = $this->getDoctrine()->getManager();
+            $userRepo=$em->getRepository('AppBundle:User');
+
+
+            $pageSize = $data["pageSize"];
+            $searchQuery = filter_var($data["searchQuery"], FILTER_SANITIZE_STRING);
+            $emailQuery = filter_var($data["emailQuery"], FILTER_SANITIZE_STRING);
+            $pageNumber = $data["pageNumber"];
+            $sort = $data["sort"];
+
+
+            $totalNumber = $userRepo->getAdminUserSearchNumber($searchQuery,$emailQuery);
+            $users = $userRepo->getAdminUserSearchResult($searchQuery,$emailQuery, $pageNumber, $pageSize,$sort);
+
+            $data = array(
+                'totalUsers' => $users ,
+                'totalNumber' => $totalNumber
+            );
+
+            return $this->_createJsonResponse('success', array('successData'=>array('users'=>$data)), 200);
+        }else{
+            return $this->_createJsonResponse('error', array('errorTitle'=>"You are not authorized to see this page."), 400);
+        }
+
+    }
+
+    /**
+     * Admin Update User Data
+     */
+    public function adminUpdateUserDataAction(Request $request)
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $userRepo = $em->getRepository('AppBundle:User');
+
+        $request_data = json_decode($request->getContent(),true);
+
+
+        if(in_array('ROLE_ADMIN_USER',$user->getRoles(),true)){
+            $editedUser = $userRepo->findOneBy(array("id" => $request_data['userId']));
+
+            if ($editedUser != null) {
+
+                if ($userRepo->checkIfUsernameExistByUsername($request_data['username'], $editedUser->getUsername())) {
+
+                    return $this->_createJsonResponse('error',array(
+                        'errorTitle'=>"Can't Approve User",
+                        'errorDescription'=> "Username '" . $request_data['username'] . "' Already Exist",
+                        'errorData'=> array(
+                            'username'=> $user->getUsername()
+                        )
+                    ),400);
+
+                } else {
+
+                    $editedUser->setUserName($request_data['username']);
+                    $editedUser->setEnabled($request_data['enabled']);
+                    $editedUser->setAdminApproved('Yes');
+                    $em->persist($editedUser);
+                    $em->flush();
+
+                    $logData = array(
+                        'user'=>$user->getId(),
+                        'logType'=>"Update User",
+                        'logDateTime'=>gmdate('Y-m-d H:i:s'),
+                        'logDescription'=> $editedUser->isEnabled()?$user->getUsername()." has updated & activated user named ".$editedUser->getUsername():$user->getUsername()." has updated & deactivated user named ".$editedUser->getUsername(),
+                        'userIpAddress'=>$this->container->get('request')->getClientIp(),
+                        'logUserType'=> in_array("ROLE_ADMIN_USER",$user->getRoles())?"Admin User":"Normal User"
+                    );
+                    $this->_saveLog($logData);
+
+                    return $this->_createJsonResponse('success',array(
+                        'successTitle'=>"User Approved",
+                    ),200);
+
+                }
+
+            }
+        }else{
+            return $this->_createJsonResponse('error', array('errorTitle'=>"You are not authorized to see this page."), 400);
+        }
+
+
+    }
+
+    /**
+     * Approve Users
+     */
+    public function approveUsersAction(Request $request){
+        $content = $request->getContent();
+        $data = json_decode($content, true);
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $userRepo = $em->getRepository('AppBundle:User');
+
+
+        if(in_array('ROLE_ADMIN_USER',$user->getRoles(),true)){
+
+            if(count($data)>0){
+                $userRepo->approveUsers($data);
+
+                $users = "";
+                foreach($data as $userRow){
+                    $users.=$userRow['username'].", ";
+                }
+
+                $logData = array(
+                    'user'=>$user->getId(),
+                    'logType'=>"Approve User",
+                    'logDateTime'=>gmdate('Y-m-d H:i:s'),
+                    'logDescription'=> $user->getUsername()." has approved users named ".$users,
+                    'userIpAddress'=>$this->container->get('request')->getClientIp(),
+                    'logUserType'=> in_array("ROLE_ADMIN_USER",$user->getRoles())?"Admin User":"Normal User"
+                );
+                $this->_saveLog($logData);
+
+                return $this->_createJsonResponse('success',array(
+                    'successTitle'=>"Users been Approved",
+                ),200);
+            }else{
+                return $this->_createJsonResponse('error', array('errorTitle'=>"No User was approved."), 400);
+            }
+
+
+        }else{
+            return $this->_createJsonResponse('error', array('errorTitle'=>"You are not authorized to see this page."), 400);
+        }
+
+
+    }
+
+    /**
+     * Add Admin Users
+     */
+    public function addAdminUserAction(Request $request)
+    {
+
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        if(in_array('ROLE_ADMIN_USER',$user->getRoles(),true)){
+
+            $formHandler = $this->container->get('fos_user.registration.form.handler');
+
+            $form = $this->container->get('fos_user.registration.form');
+
+            $form->remove('googleId');
+            $form->remove('facebookId');
+            $form->remove('googleEmail');
+            $form->remove('facebookEmail');
+            $form->remove('registrationStatus');
+            $form->remove('registrationStatus');
+            $form->remove('googleToken');
+            $form->remove('facebookToken');
+            $form->remove('campus');
+            $form->remove('referral');
+            $form->remove('googleToken');
+
+
+            $confirmationEnabled = false;
+
+
+            $process = $formHandler->process($confirmationEnabled);
+
+            if ($process) {
+
+                $em = $this->getDoctrine()->getManager();
+
+                $addedUser = $form->getData();
+                $addedUser->addRole("ROLE_ADMIN_USER");
+
+                $em->persist($addedUser);
+                $em->flush();
+
+                $logData = array(
+                    'user'=>$user->getId(),
+                    'logType'=>"Add Admin User",
+                    'logDateTime'=>gmdate('Y-m-d H:i:s'),
+                    'logDescription'=> $user->getUsername()." has created another admin user named ".$addedUser->getUsername(),
+                    'userIpAddress'=>$this->container->get('request')->getClientIp(),
+                    'logUserType'=> in_array("ROLE_ADMIN_USER",$user->getRoles())?"Admin User":"Normal User"
+                );
+                $this->_saveLog($logData);
+
+                $addedUserData=array(
+                    'email'=>$addedUser->getEmail(),
+                    'enabled'=>$addedUser->isEnabled(),
+                    'fullName'=>$addedUser->getFullName(),
+                    'roles'=>$addedUser->getRoles(),
+                    'userId'=>$addedUser->getId(),
+                    'username'=>$addedUser->getUsername(),
+                    'profilePicture'=>$addedUser->getProfilePicture()
+                );
+
+                return $this->_createJsonResponse('success', array(
+                    'successTitle'=>"Admin User Added",
+                    'successData'=>$addedUserData
+                ), 201);
+
+
+            }else{
+                return $this->_createJsonResponse('error', array(
+                    'errorTitle' => "Admin User Couldn't be created",
+                    'errorDescription' => "Sorry we were unable to add admin user. Reload the page and try again.",
+                    'errorData' => $form
+                ), 400);
+            }
+
+
+        }else{
+            return $this->_createJsonResponse('error', array('errorTitle'=>"You are not authorized to see this page."), 400);
+        }
+
+    }
+
+    public function _saveLog($logData){
+        $em = $this->container->get('doctrine')->getManager();
+        $log = new Log();
+        $logForm = $this->container->get('form.factory')->create(new LogType(), $log);
+
+        $logForm->submit($logData);
+        if($logForm->isValid()){
+            $em->persist($log);
+            $em->flush();
+        }
+    }
+
+    public function _createJsonResponse($key, $data,$code)
+    {
+        $serializer = $this->container->get('jms_serializer');
+        $json = $serializer->serialize([$key => $data], 'json');
+        $response = new Response($json, $code);
+        return $response;
+    }
+}
